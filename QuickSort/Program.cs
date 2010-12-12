341135214c3e9fdfaf4ccd6953b969e9dfbb3c5e
lucaminudel <luca@minudel>
2010-12-12 05:05:35 +0100

    Implemented: Acceptance tests

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace QuickSort
 {
@@ -9,6 +7,36 @@ namespace QuickSort
 	{
 		static void Main(string[] args)
 		{
+			Console.Write("Inserisci l'array di interi da ordinare (es: 1, 3, -10, 33): ");
+			string input = Console.ReadLine();
+			string[] numbers = input.Split(',');
+			
+			List<int> validNumgersAsInt = new List<int>();
+			foreach (var s in numbers)
+			{
+
+				try
+				{
+					int i = int.Parse(s);
+					validNumgersAsInt.Add(i);
+				}
+				catch (FormatException)
+				{
+					continue;
+				}
+			}
+
+			var quickSort = new RecursiveQuickSort();
+
+			int[] arrayToBeSorted = validNumgersAsInt.ToArray();
+			quickSort.Sort(arrayToBeSorted);
+
+			foreach (var i in arrayToBeSorted)
+			{
+				Console.Write(i + " ");
+			}
+
+			Console.WriteLine();
 		}
 	}
 }
